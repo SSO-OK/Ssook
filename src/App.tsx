118ea726d6c7a98319ea7f989276ac92ddf320cd
yunse0708 <yunse070708@gmail.com>
2024-07-04 02:01:04 +0900

    FIX : start창에서 header 숨기기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Start from "./pages/Start";
 import Header from "./components/header";
 
 function App() {
+  const location = useLocation();
+
+  const shouldShowHeader = location.pathname !== "/";
+
   return (
     <>
-      <Header />
+      {shouldShowHeader && <Header />}
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/Home" element={<Home />} />
